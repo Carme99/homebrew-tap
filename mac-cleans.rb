ed08997532a0063a19d00a51c7174af7fa865fd2
Jack Lee <me@jacklee.co.uk>
2026-03-20 22:25:48 +0000

    Update mac-cleans to v5.0 with shell completions

--- mac-cleans.rb
+++ mac-cleans.rb
@@ -1,12 +1,15 @@
 class MacCleans < Formula
   desc "macOS disk cleanup utility - free 10-50GB"
   homepage "https://github.com/Carme99/MacCleans.sh"
-  url "https://github.com/Carme99/MacCleans.sh/archive/refs/tags/v4.3.0.tar.gz"
-  sha256 "4862a43bf46580abff1a1ca98a72bc73ef6dfe9dbc614d6134be075c8712faf5"
+  url "https://github.com/Carme99/MacCleans.sh/archive/refs/tags/v5.0.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   def install
     bin.install "clean-mac-space.sh" => "mac-cleans"
+    bash_completion.install "completions/mac-cleans.bash"
+    zsh_completion.install "completions/_mac-cleans"
+    fish_install Completions: "completions/mac-cleans.fish"
   end
 
   test do
