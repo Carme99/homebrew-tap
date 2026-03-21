1c28964da698a9e1e93417cb862f6c97f7524801
Jack Lee <me@jacklee.co.uk>
2026-03-21 21:15:04 +0000

    Update mac-cleans to v5.1

--- mac-cleans.rb
+++ mac-cleans.rb
@@ -1,8 +1,8 @@
 class MacCleans < Formula
   desc "macOS disk cleanup utility - free 10-50GB"
   homepage "https://github.com/Carme99/MacCleans.sh"
-  url "https://github.com/Carme99/MacCleans.sh/archive/refs/tags/v5.0.tar.gz"
-  sha256 "092891785bbefaabfe9b77ef6e0a97b54261f4966d37e11ac2cbebf97d380d59"
+  url "https://github.com/Carme99/MacCleans.sh/archive/refs/tags/v5.1.tar.gz"
+  sha256 "319f182f929c7a07b74e74723a2c851734939e8ef14bb646834018c9f370feb8"
   license "MIT"
 
   def install
