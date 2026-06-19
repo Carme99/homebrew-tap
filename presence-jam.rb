ccaf1e0862f3312f9d1906b52da83094d6ddd18f
Nova <nova@lee-lab.local>
2026-06-19 15:21:43 +0000

    presence-jam: bump to v2.7.1

--- presence-jam.rb
+++ presence-jam.rb
@@ -1,9 +1,9 @@
 class PresenceJam < Formula
   desc "Spotify to Teams Status Sync"
   homepage "https://github.com/Carme99/PresenceJam-Desktop"
-  url "https://github.com/Carme99/PresenceJam-Desktop/releases/download/v2.7.0/PresenceJam-macos.dmg"
-  sha256 "b9429c50ba714408b3b97f6fe1d3c24f2f3369785c17f673bee4da8fdde36bd7"
-  version "2.7.0"
+  url "https://github.com/Carme99/PresenceJam-Desktop/releases/download/v2.7.1/PresenceJam-macos.dmg"
+  sha256 "f18937350e592385d56529568a8a94fbcb346d743601a8eb9344e2867cbfe014"
+  version "2.7.1"
   license "MIT"
 
   # Tauri-built macOS DMG. The DMG handler exposes the mount's root
